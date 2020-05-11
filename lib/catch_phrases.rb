6efe59421eab257d5c8d8c193aa227b0f13e4830
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 00:04:42 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -11,6 +11,12 @@ def phrase
   return "It's-a me, Mario!"
 end
 
-def all_phrases(phrase)
-  puts phrase
+def link
+  return "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases
+  mario
+  toadstool
+  link
 end
